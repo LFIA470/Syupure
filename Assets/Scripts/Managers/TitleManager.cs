a76dbec17138cd2f180b303b99d79f2e21d68cbc
LFIA470 <td0518vb@gmail.com>
2026-01-27 14:07:15 +0900

    タイトルアニメーション作成

--- Assets/Scripts/Managers/TitleManager.cs
+++ Assets/Scripts/Managers/TitleManager.cs
@@ -1,10 +1,31 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour
 {
+    [SerializeField] private Animator starAnimator;
+
+    void Start()
+    {
+        StartCoroutine(PlayStarAnimationDelay());
+    }
+
+    public void PlayShootingStar()
+    {
+        if (starAnimator != null)
+        {
+            starAnimator.SetTrigger("PlayStar");
+        }
+    }
+
+    IEnumerator PlayStarAnimationDelay()
+    {
+        yield return new WaitForSeconds(1.5f);
+
+        PlayShootingStar();
+    }
+
     public void OnStartDataButtonClicked()  //スタートボタン（画面）が押されたら呼ばれる
     {
         Debug.Log("ゲームを開始します！");
